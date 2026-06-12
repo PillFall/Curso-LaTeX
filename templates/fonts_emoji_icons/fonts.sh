66747471c6858613e740c8b30409b0c89ca0e51c
Joar Esteban Buitrago Carrillo <jebuitragoc@unal.edu.co>
2026-06-12 17:59:27 -0500

    Fix wrong display of color emoji on Linux machines

--- templates/fonts_emoji_icons/fonts.sh
+++ templates/fonts_emoji_icons/fonts.sh
@@ -3,7 +3,7 @@
 FONT_FOLDER="fonts"
 mkdir -p "$FONT_FOLDER"
 
-MANIFEST=$(curl -GsL "https://fonts.google.com/download/list" --data-urlencode "family=Noto Sans,Noto Sans Math,Noto Emoji,Noto Color Emoji,Material Symbols Outlined,Material Symbols Rounded,Material Symbols Sharp")
+MANIFEST=$(curl -GsL "https://fonts.google.com/download/list" --data-urlencode "family=Noto Sans,Noto Sans Math,Noto Emoji,Material Symbols Outlined,Material Symbols Rounded,Material Symbols Sharp")
 MANIFEST="{${MANIFEST#*{}"
 
 FONT_LIST=$(cat <<EOL
@@ -13,7 +13,6 @@ Noto_Sans/static/NotoSans-Bold.ttf|NotoSans-Bold.ttf
 Noto_Sans/static/NotoSans-BoldItalic.ttf|NotoSans-BoldItalic.ttf
 Noto_Sans_Math/NotoSansMath-Regular.ttf|NotoSansMath.ttf
 Noto_Emoji/NotoEmoji-VariableFont_wght.ttf|NotoEmoji.ttf
-Noto_Color_Emoji/NotoColorEmoji-Regular.ttf|NotoColorEmoji.ttf
 Material_Symbols_Outlined/MaterialSymbolsOutlined-VariableFont_FILL,GRAD,opsz,wght.ttf|MaterialSymbolsOutlined.ttf
 Material_Symbols_Rounded/MaterialSymbolsRounded-VariableFont_FILL,GRAD,opsz,wght.ttf|MaterialSymbolsRounded.ttf
 Material_Symbols_Sharp/MaterialSymbolsSharp-VariableFont_FILL,GRAD,opsz,wght.ttf|MaterialSymbolsSharp.ttf
@@ -31,5 +30,7 @@ do
 
     curl -sL "$FONT_URL" -o "$FONT_FOLDER/$TARGET_NAME"
 done
+
+curl -sL "https://github.com/googlefonts/noto-emoji/raw/refs/heads/main/fonts/NotoColorEmoji_WindowsCompatible.ttf" -o "$FONT_FOLDER/NotoColorEmoji.ttf"
 
 echo "Downloaded all fonts to '$FONT_FOLDER' folder"

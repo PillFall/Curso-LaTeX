ef86cc3bbaeda76e2cbc1e400be801b7540d4b85
Joar Esteban Buitrago Carrillo <jebuitragoc@unal.edu.co>
2026-06-19 06:02:43 -0500

    Fix error in font download script on windows

--- templates/fonts_emoji_icons/fonts.ps1
+++ templates/fonts_emoji_icons/fonts.ps1
@@ -11,27 +11,23 @@ $FontFolder = "fonts"
 New-Item -Path $FontFolder -ItemType Directory -Force | Out-Null
 
 $NotoFonts = @(
-    @{ filename = "Noto_Sans/static/NotoSans-Regular.ttf"; path = "NotoSans-Regular.ttf" },
-    @{ filename = "Noto_Sans/static/NotoSans-Italic.ttf"; path = "NotoSans-Italic.ttf" },
-    @{ filename = "Noto_Sans/static/NotoSans-Bold.ttf"; path = "NotoSans-Bold.ttf" },
-    @{ filename = "Noto_Sans/static/NotoSans-BoldItalic.ttf"; path = "NotoSans-BoldItalic.ttf" },
-    @{ filename = "Noto_Sans_Math/NotoSansMath-Regular.ttf"; path = "NotoSansMath.ttf" },
-    @{ filename = "Noto_Emoji/NotoEmoji-VariableFont_wght.ttf"; path = "NotoEmoji.ttf" },
-    @{ filename = "Material_Symbols_Outlined/MaterialSymbolsOutlined-VariableFont_FILL,GRAD,opsz,wght.ttf"; path = "MaterialSymbolsOutlined.ttf" },
-    @{ filename = "Material_Symbols_Rounded/MaterialSymbolsRounded-VariableFont_FILL,GRAD,opsz,wght.ttf"; path = "MaterialSymbolsRounded.ttf" },
-    @{ filename = "Material_Symbols_Sharp/MaterialSymbolsSharp-VariableFont_FILL,GRAD,opsz,wght.ttf"; path = "MaterialSymbolsSharp.ttf" }
+    @{ filename = "Noto_Sans/static/NotoSans-Regular.ttf"; path = "$FontFolder\NotoSans-Regular.ttf" },
+    @{ filename = "Noto_Sans/static/NotoSans-Italic.ttf"; path = "$FontFolder\NotoSans-Italic.ttf" },
+    @{ filename = "Noto_Sans/static/NotoSans-Bold.ttf"; path = "$FontFolder\NotoSans-Bold.ttf" },
+    @{ filename = "Noto_Sans/static/NotoSans-BoldItalic.ttf"; path = "$FontFolder\NotoSans-BoldItalic.ttf" },
+    @{ filename = "Noto_Sans_Math/NotoSansMath-Regular.ttf"; path = "$FontFolder\NotoSansMath.ttf" },
+    @{ filename = "Noto_Emoji/NotoEmoji-VariableFont_wght.ttf"; path = "$FontFolder\NotoEmoji.ttf" },
+    @{ filename = "Material_Symbols_Outlined/MaterialSymbolsOutlined-VariableFont_FILL,GRAD,opsz,wght.ttf"; path = "$FontFolder\MaterialSymbolsOutlined.ttf" },
+    @{ filename = "Material_Symbols_Rounded/MaterialSymbolsRounded-VariableFont_FILL,GRAD,opsz,wght.ttf"; path = "$FontFolder\MaterialSymbolsRounded.ttf" },
+    @{ filename = "Material_Symbols_Sharp/MaterialSymbolsSharp-VariableFont_FILL,GRAD,opsz,wght.ttf"; path = "$FontFolder\MaterialSymbolsSharp.ttf" }
 )
-
 
 foreach ($NotoFont in $NotoFonts) {
     $NotoFontFileRef = $FileRefs.Where({ $_.filename -eq $NotoFont.filename })
     Invoke-RestMethod -UseBasicParsing -Uri $NotoFontFileRef.url -OutFile $FontFolder\$NotoFont.path
 }
 
-
-
 # Windows tiene un formato especial para las fuentes con emojis, por lo que hay que utilizar otro archivo diferente compatible con Windows
-
 Invoke-RestMethod -Uri "https://github.com/googlefonts/noto-emoji/raw/refs/heads/main/fonts/NotoColorEmoji_WindowsCompatible.ttf" -OutFile "$FontFolder\NotoColorEmoji.ttf"
 
 Write-Host "Downloaded all fonts to '$FontFolder' folder"
